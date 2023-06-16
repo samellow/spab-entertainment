5a4868b207af7f65e2790a303fb4a90061e95a8d
samuel <sammykaris48@gmail.com>
2023-06-16 13:37:14 +0300

    updated

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 
-function submitScores() {
-   
-    let gamer1 = document.getElementById("gamer1").value;
-    let gamer2 = document.getElementById("gamer2").value;
-    let score1 = document.getElementById("score1").value;
-    let score2 = document.getElementById("score2").value;
-    // Perform any desired operations with the scores
-    // For example, you can display them on the page
-    document.getElementById("result").innerHTML = "Score: " + gamer1 + " " + score1 + " - " + score2 + " " + gamer2;
-  }
+const form = document.querySelector('form');
 
-  console.log(gamer1, gamer2)
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const formData = new FormData(form);
+  const formObject = Object.fromEntries(formData);
+  const json = JSON.stringify(formObject);
+  localStorage.setItem('form',json);
+  console.log(formObject);
+
+})
+  
+
+ 
 
   
  
